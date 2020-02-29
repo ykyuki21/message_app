315fd7f9bc16cb3d834040c6157cd3339ed91d43
ykyuki21 <yk.yuki21@gmail.com>
2020-02-29 21:31:02 +0900

    Implement login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :is_logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def is_logged_in?
+    !!current_user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
   def index
-    
+    redirect_to root_path if is_logged_in?
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "ログインしました。"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "ログインに失敗しました。"
+      render 'index'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_path
   end
 end
