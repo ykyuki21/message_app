5d721744f62495c424e8b031eda5fa5e9dcde9be
ykyuki21 <yk.yuki21@gmail.com>
2020-03-02 01:52:41 +0900

    Add action cable to chatroom and fix layouts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,17 @@ class MessagesController < ApplicationController
   
   def create
     message = current_user.messages.build(message_params)
-    message.save
+    if message.save
+      ActionCable.server.broadcast 'chatroom_channel', content: render_message(message)
+    end
   end
 
   private
     def message_params
       params.require(:message).permit(:body)
+    end
+
+    def render_message message
+      render partial: 'message', locals: {message: message}
     end
 end
